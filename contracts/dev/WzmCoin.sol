e5f500ef8689061ee20ef67f7eff322e116a24ed
EndOfMaster <843698826@qq.com>
2020-05-25 16:38:09 +0800

    完成一个转账。。。20都不算。。这个吧付的钱存到我这然后实现转账，算是中转场

--- contracts/dev/WzmCoin.sol
+++ contracts/dev/WzmCoin.sol
@@ -1,11 +1,31 @@
-pragma solidity ^0.5.8;
+pragma solidity ^0.6.8;
 
-import "../lib/Ownable.sol";
+contract WzmCoin {
 
-contract WzmCoin is Ownable {
+    mapping(address => uint256) private balances;
 
-    mapping(address => uint) public balances;
+    function addMoney() public payable {
+        balances[msg.sender] += msg.value;
+    }
 
-    function 
+    modifier hasMoney(uint money) {
+        //这里不是true触发，而是false，语义应该是应该怎么样，否则。。
+        require(balances[msg.sender] > money, "not have this money");
+        _;
+    }
+
+    function getMoney(uint256 money) public payable hasMoney(money){
+        balances[msg.sender] -= money;
+        msg.sender.transfer(money);
+    }
+
+    function transfer(address payable to, uint256 money) public payable hasMoney(money) {
+        balances[msg.sender] -= money;
+        balances[to] += money;
+    }
+
+    function showMoney() view public returns (uint256 money){
+        return balances[msg.sender];
+    }
 
 }
--- contracts/lib/Ownable.sol
+++ contracts/lib/Ownable.sol
@@ -16,7 +16,7 @@
 
 */
 
-pragma solidity ^0.5.8;
+pragma solidity ^0.6.8;
 
 /**
  * Library to handle user permissions.

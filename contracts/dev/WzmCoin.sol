5e2182654860723458732e382afe4ab3679b25de
EndOfMaster <843698826@qq.com>
2020-05-27 17:46:50 +0800

    根据remix修改20接口的错误和警告，编写20的基础方法，铸造和销毁明天

--- contracts/dev/WzmCoin.sol
+++ contracts/dev/WzmCoin.sol
@@ -2,7 +2,99 @@
 
 pragma solidity ^0.6.8;
 
-contract WzmCoin{
+import "./interface/IERC20.sol";
+import "../lib/Ownable.sol";
 
+contract WzmCoin is IERC20, Ownable {
+
+    //20的本质是质押eth根据设定比例生成的token，所以总量在最开始的时候是0
+    uint256 private _totalSupply;
+
+    mapping(address => uint256) private _balances;
+
+    //谁委托到谁多少币
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    string private _name;
+
+    string private _symbol;
+
+    uint8 private _decimals;
+
+    constructor() public {
+        _name = "Wzm Coin";
+        _symbol = "WC";
+        _decimals = 4;
+    }
+
+    modifier notZero(address _to) {
+        require(_to != address(0), "transfer to the zero address");
+        _;
+    }
+
+    modifier notZeroDouble(address _from, address _to) {
+        require(_from != address(0), "transfer from the zero address");
+        require(_to != address(0), "transfer to the zero address");
+        _;
+    }
+
+    modifier hasMoney(uint256 _value) {
+        require(_balances[msg.sender] > _value, "not have this money");
+        _;
+    }
+
+    function totalSupply() public view override returns (uint256){
+        return _totalSupply;
+    }
+
+    function name() public view returns (string memory){
+        return _name;
+    }
+
+    function symbol() public view returns (string memory){
+        return _symbol;
+    }
+
+    function decimals() public view returns (uint8){
+        return _decimals;
+    }
+
+    function transfer(address _to, uint256 _value) public override notZero(_to) hasMoney(_value) returns (bool) {
+        _balances[msg.sender] -= _value;
+        _balances[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function balanceOf(address _owner) public view override notZero(_owner) returns (uint256){
+        return _balances[_owner];
+    }
+
+    function allowance(address _owner, address _spender) public view override notZeroDouble(_owner, _spender) returns (uint256){
+        return _allowances[_owner][_spender];
+    }
+
+    function approve(address _spender, uint256 _value) public override notZero(_spender) hasMoney(_value) returns (bool){
+        _allowances[msg.sender][_spender] += _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public override notZeroDouble(_from, _to) returns (bool){
+        require(_allowances[_from][msg.sender] > _value, "Not so much allowance");
+        require(_balances[_from] > _value, "not have this money");
+        _balances[_from] -= _value;
+        _balances[_to] += _value;
+        _allowances[_from][msg.sender] -= _value;
+        return true;
+    }
+
+    //todo
+    function _mint(address account, uint256 amount) internal {
+    }
+
+    //todo
+    function _burn(address account, uint256 amount) internal virtual {
+    }
 
 }
--- contracts/dev/interface/IERC20.sol
+++ contracts/dev/interface/IERC20.sol
@@ -8,21 +8,21 @@ pragma solidity ^0.6.8;
 interface IERC20 {
 
     //总量
-    function totalSupply() constant returns (uint256 totalSupply);
+    function totalSupply() external view returns (uint256);
 
     //某用户的余额
-    function balanceOf(address _owner) constant returns (uint256 balance);
+    function balanceOf(address _owner) external view returns (uint256 balance);
 
     //转账
-    function transfer(address _to, uint256 _value) returns (bool success);
+    function transfer(address _to, uint256 _value) external returns (bool success);
 
-    //和approve完成委托交易，a使用approve委托多少coin然后，b帮a用这个方法卖给c
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
+    //和approve完成委托交易，a使用approve委托多少coin给b,然后b帮a用这个方法卖给c,msg.sender应该是b
+    function transferFrom(address _from, address _to, uint256 _value) external returns (bool success);
 
-    function approve(address _spender, uint256 _value) returns (bool success);
+    function approve(address _spender, uint256 _value) external returns (bool success);
 
     //查询a委托到b多少coin
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining);
+    function allowance(address _owner, address _spender) external view returns (uint256 remaining);
 
     //交易通知
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
